1588cea411e1ff94eeded9532ae9e8a86a70388b
Myles <mylesjanik@gmail.com>
2020-11-01 15:13:52 -0800

    commit tree d4afd465903be3767fad8a6c42f3b6a6358a2344

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+    is_true = true
+    i = 2
+    new_int = int.abs
+    while i < new_int
+        if new_int % i == 0
+            is_true = false
+        end
+        i += 1
+    end
+    if new_int == 1 || new_int == 0
+        is_true = false
+    end
+    return is_true
+end
